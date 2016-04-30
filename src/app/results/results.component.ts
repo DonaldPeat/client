f167f3767483805b9b804341a1c15ee9ba437724
Jeffrey <jeguo@ucsc.edu>
2016-04-30 15:55:01 -0700

    added getNextRound and getPrevRound

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from 'angular2/core';
 import { Poll } from '../models/poll';
-import { Candidate } from '../models/candidate';
+import {Candidate} from '../models/candidate';
 import { Vote } from '../models/vote';
 import { OrderedSet } from "immutable";
 
@@ -12,9 +12,10 @@ import { OrderedSet } from "immutable";
 @Component({
   selector: 'results-inner',
   template: `
-      <div>We have a total vote of: {{getTotalVotes()}}</div>
-    <div *ngFor="#cand of poll.candidates">
-      <div>{{cand.name}}: <span>{{getVotes(cand)}}</span> </div>
+     <div><span><Button (click)="getPrevRound();">Previous Round</Button></span><Button (click)="getNextRound();" >Next Round</Button></div>
+      <div>The total number of votes: {{getTotalVotes()}}</div>
+    <div *ngFor="#cand of poll.candidates; #i = index">
+      <div>{{cand.name}}: <span>{{candVotes[i]}}</span> </div>
     </div>
 
   `
@@ -23,6 +24,9 @@ export class ResultsDumbComponent implements OnInit {
   @Input() poll: Poll;
 
   private votes: Vote[];
+  private candVotes: number[];
+  private candNames: string[];
+  private resultHistory: Object[];
 
   ngOnInit(){
     let cands = this.poll.candidates.toJS(),
@@ -30,27 +34,67 @@ export class ResultsDumbComponent implements OnInit {
 
     this.votes = this.poll.votes.toJS();
 
+    this.candNames = new Array(cands.length);
+
+    for (let i = 0 ; i < cands.length ; i++) {
+      this.candNames[i] = cands[i].id;
+    }
+
+    this.candVotes = this.getVotes(this.candNames);
+
+    this.resultHistory = [];
+
     debugger;
   }
 
-  getVotes(candidate: Candidate): number {
-    var sum = 0;
+  getVotes(candIds: string[]): number[] {
+    var sum = new Array(candIds.length).fill(0);
 
     for ( let userVotes of this.votes ) {
-      var userChoices:OrderedSet<String> = userVotes.choices;
+      var userChoices: OrderedSet<String> = userVotes.choices;
 
-      userChoices.forEach((voteId) => {
-        if (voteId == candidate.id) {
-          sum = sum + 1;
+      for ( let n of userChoices ) {
+        let number = candIds.indexOf(n);
+        if ( number != -1 ) {
+          sum[number] += 1;
+          break;
         }
-        return true;
-      });
+      }
+
     }
+
     return sum;
   }
 
+  getNextRound() {
+    this.resultHistory.push({
+      NameArray: this.candNames,
+      VoteArray: this.candVotes
+    });
+    console.log(this.resultHistory);
+
+    var minValue = Math.min.apply(Math,this.candVotes.filter( x => { return x !== 0; }));
+
+    for( let i = 0 ; i < this.candVotes.length ; i++ ) {
+      if (this.candVotes[i] == minValue || this.candVotes[i] == 0) {
+        this.candNames[i] = null;
+      }
+    }
+
+    this.candVotes = this.getVotes(this.candNames);
+
+  }
+
+  getPrevRound() {
+    if (this.resultHistory.length != 0) {
+      var prevRound = this.resultHistory.pop();
+      this.candNames = prevRound.NameArray;
+      this.candVotes = prevRound.VoteArray;
+      console.log("test2",this.resultHistory);
+    }
+  }
+
   getTotalVotes() {
-    console.log("test2");
     return this.votes.length;
   }
   
