98d5df963536fac09d9f3c3ca2a7fecadead17e1
Jeffrey <jeguo@ucsc.edu>
2016-04-30 22:06:49 -0700

    Added comments, fixed End Result function, implemented From Start (restart simulation) function

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input, OnInit } from 'angular2/core';
 import { Poll } from '../models/poll';
-import {Candidate} from '../models/candidate';
 import { Vote } from '../models/vote';
 import { OrderedSet } from "immutable";
+import * as _ from 'lodash';
 
 /**
  * This is the "dumb component" - it's "dumb" because it doesn't know anything about the rest of the application. It just 
@@ -14,6 +14,7 @@ import { OrderedSet } from "immutable";
   template: `
      <div>
         <span>
+            <button (click)="progressToStart();">From Start</button>
             <button (click)="progressToPrevRound();">Previous Round</button>
             <button (click)="progressToNextRound();" >Next Round</button>
             <button (click)="progressToWinner();">End Result</button>
@@ -22,7 +23,7 @@ import { OrderedSet } from "immutable";
      
       <div>The total number of votes: {{getTotalVotes()}}</div>
       <div *ngFor="#cand of poll.candidates #i = index">
-        <div>{{cand.name}}: <span>{{candVotes[i]}}</span> </div>
+        <div>{{cand.name}}: <span>{{candVotes[i]}}</span></div>
     </div>
 
   `
@@ -40,17 +41,27 @@ export class ResultsDumbComponent implements OnInit {
     let cands = this.poll.candidates.toJS(),
         votes = this.poll.votes.toJS();
 
+    //setup
+
+    //Holds all the candidates' ids
     this.candIds = cands.map(cand => cand.id);
+    //Holds all the candidates' ids, but won't be modified
     this.originalCandIds = cands.map(cand => cand.id);
 
+    //All the vote objects from the current poll
     this.votes = this.poll.votes.toJS();
 
+    //An array of votes (number) corresponding to the array of candIds
     this.candVotes = this.getVotes(this.candIds);
 
+    //A log to keep track of different progresses
     this.progressHistory = [];
 
   }
 
+  /* @Param Takes in Candidate Id's
+   * @Return An array of number of votes corresponding to the array of Candidate Ids
+   */
   getVotes(candIds: string[]): number[] {
     var candVoteResult = new Array(candIds.length).fill(0);
 
@@ -69,21 +80,26 @@ export class ResultsDumbComponent implements OnInit {
     return candVoteResult;
   }
 
+  /*
+   * eliminates candidate(s) according to the votes
+   * from this.candVotes,and updates the votes at the end
+   */
   progressToNextRound() {
-    var highestVoteValue = Math.max.apply(Math,this.candVotes);
-    console.log(this.candIds);
+    var highestVoteValue = Math.max(...this.candVotes);
 
+    //Checks if we have found a winner
     if ( this.getTotalVotes()*0.5 > highestVoteValue ) {
 
       //Clone-ing the object here, otherwise it would just modify the object.
-      this.progressHistory.push({
+      this.progressHistory.push(JSON.parse(JSON.stringify({
         candIdsArray: this.candIds,
         candVotesArray: this.candVotes
-      });
+      })));
 
       var lowestVoteValue = Math.min(...this.candVotes.filter(x => {return x !== 0;}));
 
       for (let i = 0; i < this.candVotes.length; i++) {
+        //Checks if there are repeating lowest vote value
         if (this.candVotes[i] == lowestVoteValue || this.candVotes[i] == 0) {
           this.candIds[i] = null;
         }
@@ -94,27 +110,37 @@ export class ResultsDumbComponent implements OnInit {
 
     }
 
-
   }
 
+  /* Using progressHistory to load the previous
+   * this.candIds and this.candVotes
+   */
   progressToPrevRound() {
     if (this.progressHistory.length != 0) {
       var prevRound = this.progressHistory.pop();
       this.candIds = prevRound.candIdsArray;
       this.candVotes = prevRound.candVotesArray;
-
     }
   }
 
+  //Call progressToNextRound until it finds a winner
   progressToWinner() {
-    var maxValue = Math.max(...this.candVotes);
-    while ( this.getTotalVotes()*0.5 > maxValue ) {
-      console.log(this.getTotalVotes()*0.5, maxValue);
+    var highestVoteValue = Math.max(...this.candVotes);
+    while ( this.getTotalVotes()*0.5 > highestVoteValue ) {
       this.progressToNextRound();
-      debugger;
+      highestVoteValue = Math.max(...this.candVotes);
     }
   }
 
+  //Restarts the simulation
+  progressToStart() {
+    //reset everything
+    this.candIds = _.clone(this.originalCandIds);
+    this.candVotes = this.getVotes(this.candIds);
+    this.progressHistory = [];
+  }
+
+  //Returns the total number of votes
   getTotalVotes(): number {
     return this.votes.length;
   }
