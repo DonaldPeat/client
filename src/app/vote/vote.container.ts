c23d955af0e61bc7cd22a8fc2b1bda4b0db5d23b
Drew Moore <drewmoore@ucsc.edu>
2016-05-03 22:59:07 -0700

    resolve remaining TS compiler errors

--- src/app/vote/vote.container.ts
+++ src/app/vote/vote.container.ts
@@ -10,37 +10,9 @@ import { Vote } from '../models/vote';
   selector: 'vote-input',
   directives: [],  
   template: `
-    <div *ngFor="#cand of candidates$ | async">
-      <div>{{cand.name}}</div>
-    </div>
+
 `
 })
-export class VoteInputContainer implements AfterViewInit {
-  poll$: Observable<Poll>;
-  candidates$: Observable<string[]>;
-  constructor(private polls: Polls){
-    this.poll$ = this.polls.load("asdf").take(1);
-    this.candidates$ = this.poll$.map(function(poll){
-    return poll.candidates;
-  });
+export class VoteInputContainer {
 
-    this.candidates$ = this.poll$.map(poll => poll.candidates);
-
-    this.poll$.subscribe(poll => {
-      
-      //.toJS() just converts from immutable collections to plain JS arrays so it's easy to play with them
-      let cands: Candidate[] = poll.candidates.toJS(),
-          votes: Vote[] = poll.votes.toJS();
-
-      debugger;
-    });
-
-  }
-
-
-  ngAfterViewInit() {
-
-
-
-  };
 }
