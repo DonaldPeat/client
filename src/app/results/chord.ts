7310a1efed869c4bbbef8768a65b4260fe7fe156
Drew Moore <drewmoore@ucsc.edu>
2016-05-30 00:01:10 -0700

    refactor chord

--- src/app/results/chord.ts
+++ src/app/results/chord.ts
@@ -19,8 +19,8 @@ export class ChordDiagramComponent implements OnInit, AfterViewInit {
   @Input() cands$: Observable<Candidate[]>;
   @Input() totalVotes$: Observable<number>;
 
-  private width = 500;
-  private height = 500;
+  private width;
+  private height;
   private svg: Selection<any>;
   private g: any;
   private screenWidth$: Subject<number> = BehaviorSubject.create();
@@ -43,157 +43,91 @@ export class ChordDiagramComponent implements OnInit, AfterViewInit {
      */
     Observable.combineLatest( this.cands$, this.screenWidth$, this.totalVotes$ ).subscribe(
         ([cands, width, totalVotes]) => {
-          let tot                   = cands.reduce( (sum, cand) => sum + cand.score, 1 ), //the total # of active votes
-              ids                   = cands.map( cand =>cand.id ),
-              initMatrix = ids.map((id, idx)=> {
-                let ret = Array(ids.length).fill(0);
-                ret[idx] = cands[idx].score;
-                return ret;
-              }),
+          let tot         = cands.reduce( (sum, cand) => sum + cand.score, 1 ), //the total # of active votes
+              ids         = cands.map( cand =>cand.id ).sort(),
 
-              allyVotes: number[][] = cands.reduce( (resultArr, cand) => {
+              initMatrix  = ids.map( (id, idx)=> {
+                let ret = Array( ids.length ).fill( 0 );
+                ret[ idx ] = cands[ idx ].score;
+                return ret;
+              } ),
+
+              voteMatrix  = cands.reduce( (resultArr, cand) => {
                 let allyVotes: {[id: string]: number} = cand.getInboundAllyVotes(),
                     rowIdx                            = ids.indexOf( cand.id );
                 _.keys( allyVotes ).forEach( candId => {
-
-                  let colIdx = ids.indexOf( candId ),
-                      val    = allyVotes[ candId ];
-                  resultArr[ rowIdx ][ colIdx ] = val;
+                  resultArr[ rowIdx ][ ids.indexOf( candId ) ] = allyVotes[ candId ];
                 } );
                 return resultArr
-              }, initMatrix );
+              }, initMatrix ),
 
-          console.log( JSON.stringify(allyVotes) );
-          /*
-           function getDataMatrix(dict) {
-           var outerArray = [];
-           for (var key in dict) {
-           for (var innerKey in dict[key]){
-           var innerArray=[];
-           innerArray.push(dict[key][innerKey]);
-           }
-           outerArray.push(innerArray);
-           }
-           return outerArray
-           };
+              innerRadius = Math.min( this.width, this.height ) * .41,  //outer arc blocks for labels & mouse selection
+              outerRadius = innerRadius * 1.1,
+              fill        = d3.scale.category20b().domain( ids ),
 
-           let testMatrix = getDataMatrix(allyVotes);
-           console.log(testMatrix);
-           */
-          /*                let matrix =
+              fade        = (opacity)=> {
+                return (g, i) => {
+                  this.svg.selectAll( ".chord path" )
+                      .filter( function (d) { return d.source.index != i && d.target.index != i; } )
+                      .transition()
+                      .style( "opacity", opacity );
+                }
+              },
 
-           [ //dummy data matrix for chord diagram to be replaced with candidates' votes/
-           [10000,  250, 500, 2500,23],
-           [ 5000, 300, 0, 2500,12],
-           [ 2500, 0, 0, 0,25],
-           [ 2500,  0,  0, 0,34],[529, 343, 232, 132, 455]
-           ];
-           */
-
-
-          /*
-           let matrix =
-
-           [ //dummy data matrix for chord diagram to be replaced with candidates' votes/
-           [0,  33, 0, 0,0],
-           [ 0, 27, 0, 0,0],
-           [ 0, 0, 25, 0,0],
-           [ 0,  0,  0, 0,0],[0, 0, 0, 0, 0]
-           ],
-
-           */
-
-
-          let matrix      = allyVotes,
               chord       = d3.layout.chord()
                               .padding( .05 )
                               .sortSubgroups( d3.descending )
-                              .matrix( matrix ),
-
-              height      = 500,
-              innerRadius = Math.min( this.width, this.height ) * .41,  //outer arc blocks for labels & mouse selection
-              outerRadius = innerRadius * 1.1,
-
-              /*
-               updates = this.g.selectAll( '.chord' ).data( matrix ),
-               enters  = updates.enter(), // elements we're drawing for the first time
-               exits   = updates.exit(); // elements that are being removed
-
-               enters.append(  )
-
-               } );
-               */
-
-              fill        = d3.scale.ordinal()
-                              .domain( [ d3.range( cands.length ).toString() ] )          //dom & range to be changed to #candidates/votes
-                              .range( [ "#000000", "#FFDD89", "#957244", "#F26223", "#000099", "#FEDD89", "#357244", "#F26283",
-                                        "#340000", "#DFDD89", "#956244", "#F46223", "#043000", "#FDDD89", "#959244",
-                                        "#F26273" ] ),
-
-              svg         = d3.select( this.element.nativeElement )
-                              .append( "svg" )
-                              .attr( "width", width )
-                              .attr( "height", height )
-                              .append( "g" )
-                              .attr( "transform", "translate(" + width / 2 + "," + height / 2 + ")" );
-
-          svg.append( "g" ).selectAll( "path" )
-             .data( chord.groups )
-             .enter().append( "path" )
-             .style( "fill", function (d) { return fill( d.index );} ) //from d.index
-             .style( "stroke", function (d) { return fill( d.index ); } )
-             .attr( "d", d3.svg.arc().innerRadius( innerRadius ).outerRadius( outerRadius ) )
-             .on( "mouseover", fade( .1 ) ) //fade out unselected relationships
-             .on( "mouseout", fade( 1 ) );  //display all relationships when none selected
-
-          let ticks = svg.append( "g" ).selectAll( "g" )
-                         .data( chord.groups )
-                         .attr('class', 'asdf')
-                         .enter().append( "g" ).selectAll( "g" )
-                         .data( getCandVoteAngles )
-                         .enter().append( "g" )
-                         .attr( "transform", function (d) {
-                           return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
-                                  + "translate(" + outerRadius + ",0)";
-
-                         } );
-
-          svg.append( "g" )
-             .attr( "class", "chord" )
-             .selectAll( "path" )
-             .data( chord.chords )
-             .enter().append( "path" )
-             .attr( "d", d3.svg.chord().radius( innerRadius ) )
-             .style( "fill", function (d) { return fill( d.target.index ); } )
-             .style( "opacity", 1 );
+                              .matrix( voteMatrix );
 
 
-          // Returns an array of angles and votecount labels, given candidate
-          function getCandVoteAngles(d) {
-            var k = (d.endAngle - d.startAngle) / d.value;
-            return d3.range( 0, d.value, 1000 ).map( function (v, i) {
-              return {
-                angle: v * k + d.startAngle,
-                label: i % 5 ? null : v / 1000 + "k"
-              }
-            } )
-          };
+          //the arcs that form the outer circle
+          let arcs         = this.svg.selectAll( '.cand-arc' ).data( chord.groups ),
+              enteringArcs = arcs.enter(),
+              exitingArcs  = arcs.exit();
+
+
+          //applied only for new (i.e. not-yet-drawn) arcs
+          enteringArcs.append( 'path' )
+                      .attr( 'class', 'cand-arc' )
+                      .style( "fill", function (d) { return fill( d.index );} ) //from d.index
+                      .style( "stroke", function (d) { return fill( d.index ); } )
+                      .on( "mouseover", fade( .1 ) ) //fade out unselected relationships
+                      .on( "mouseout", fade( 1 ) );  //display all relationships when none selected
+
+          //applied for all arcs
+          arcs.attr( "d", d3.svg.arc().innerRadius( innerRadius ).outerRadius( outerRadius ) );
+
+
+          let chords         = this.svg.selectAll( '.chord' ).data( chord.chords ),
+              enteringChords = chords.enter(),
+              exitingChords  = chords.exit();
+
+
+          //TODO(donald) draw the chords
+          // note: each candidate has a self-referential chord - you want to hide that.
+
 
           // Method for fading unselected candidates' data
-          function fade(opacity) {
-            return function (g, i) {
 
-              svg.selectAll( ".chord path" )
-                 .filter( function (d) { return d.source.index != i && d.target.index != i; } )
-                 .transition()
-                 .style( "opacity", opacity );
-            }
-          };
+
         } )
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.screenWidth$.next( this.element.nativeElement.clientWidth );
+
+
+    this.width = this.height = this.element.nativeElement.clientWidth;
+    this.svg = d3.select( this.element.nativeElement )
+                 .append( 'svg' )
+                 .attr( 'width', `${this.width}` )
+                 .attr( 'height', `${this.height}` )
+                 .append( 'g' )
+                 .attr( "transform", `translate(${this.width / 2 },${this.height / 2})` );
+
+
+    this.screenWidth$.next( this.width );
+
 
   }
 
